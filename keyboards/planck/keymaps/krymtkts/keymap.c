66b940f4e76ce0f7ffba03dc9411151972bf1764
krymtkts <takatoshi.kuriyama@gmail.com>
2019-01-23 15:42:19 +0900

    Enhance planck keymap.

--- keyboards/planck/keymaps/krymtkts/keymap.c
+++ keyboards/planck/keymaps/krymtkts/keymap.c
@@ -48,15 +48,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |  =   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Esc  | Del  | Alt  | GUI  | Space| Tab  | Bksp |Enter |   `  |   [  |   ]  |  \   |
+ * | Esc  | GUI  | Alt  | Del  | Space| Tab  | Bksp |Enter |   `  |   [  |   ]  |  \   |
  * |                           | Ctrl | Func | Num  |Shift |                           |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
-    CNG_IME, KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-    KC_LCTL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,     KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,     KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_EQL,
-    KC_ESC,  KC_DEL, KC_LALT, KC_LGUI, CTL_SPC, FUNC_TAB, NUM_BKSP, SFT_ENT, KC_GRV,  KC_LBRC, KC_RBRC, KC_BSLS
+    CNG_IME, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_EQL,
+    KC_ESC,  KC_LGUI, KC_LALT, KC_DEL,  CTL_SPC, FUNC_TAB, NUM_BKSP, SFT_ENT, KC_GRV,  KC_LBRC, KC_RBRC, KC_BSLS
 ),
 
 /* Colemak
@@ -67,15 +67,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |  =   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Esc  | Del  | Alt  | GUI  | Space| Tab  | Bksp |Enter |   `  |   [  |   ]  |  \   |
+ * | Esc  | GUI  | Alt  | Del  | Space| Tab  | Bksp |Enter |   `  |   [  |   ]  |  \   |
  * |                           | Ctrl | Func | Num  |Shift |                           |
  * `-----------------------------------------------------------------------------------'
  */
 [_COLEMAK] = LAYOUT_planck_grid(
-    CNG_IME, KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,     KC_J,     KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_MINS,
-    KC_LCTL, KC_A,   KC_R,    KC_S,    KC_T,    KC_D,     KC_H,     KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
-    KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,     KC_K,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_EQL,
-    KC_ESC,  KC_DEL, KC_LALT, KC_LGUI, CTL_SPC, FUNC_TAB, NUM_BKSP, SFT_ENT, KC_GRV,  KC_LBRC, KC_RBRC, KC_BSLS
+    CNG_IME, KC_Q,    KC_W,    KC_F,   KC_P,    KC_G,     KC_J,     KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_MINS,
+    KC_LCTL, KC_A,    KC_R,    KC_S,   KC_T,    KC_D,     KC_H,     KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,     KC_K,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_EQL,
+    KC_ESC,  KC_LGUI, KC_LALT, KC_DEL, CTL_SPC, FUNC_TAB, NUM_BKSP, SFT_ENT, KC_GRV,  KC_LBRC, KC_RBRC, KC_BSLS
 ),
 
 /* Function
