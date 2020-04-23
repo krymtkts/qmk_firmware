20615e66982f77052a546866b90f08048da13e50
krymtkts <takatoshi.kuriyama@gmail.com>
2020-04-23 23:10:28 +0900

    Update key iris map.

--- keyboards/keebio/iris/keymaps/krymtkts/keymap.c
+++ keyboards/keebio/iris/keymaps/krymtkts/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-    KC_LNG,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, CTL_T(KC_QUOT),
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    CTL_TB,       SFT_BS,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_T(KC_EQL),
                                         KC_LGUI, FNC_SPC, CTL_TB,       SFT_BS,  SYM_ENT, KC_RALT
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_SYMBOL] = LAYOUT(
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                        KC_CIRC, KC_AMPR,  KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,
-    RESET,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                           KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    KC_PIPE,
+    KC_LNG,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                           KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    KC_PIPE,
     _______, _______, _______, _______, KC_LPRN, KC_LBRC,                        KC_RBRC, KC_RPRN,  _______, _______, KC_COLN, KC_DQT,
     BL_STEP, BL_TOGG, BL_DEC,  BL_INC,  _______, KC_LCBR, KC_DEL,       _______, KC_RCBR, _______,  KC_LABK, KC_RABK, KC_QUES, KC_PLUS,
                                         _______, _______, KC_DEL,       _______, _______, _______
